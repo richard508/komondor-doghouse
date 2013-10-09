35e36887474139b11bb3e7da88e580cfffc7433a
Richard Almonte <richard.almonte@infosurv.com>
2013-10-08 21:33:35 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ihub::Application.config.secret_key_base = 'acb9fada9b5371f9246e4933b83364c83949cf42e3d64ae73f36d20eb09421cb57a2ed6f6a5d2f8a34181f8dfe3b5106451a5ceee2d1b0c6559bd5afc0e883d1'
