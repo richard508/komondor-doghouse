496e9fdcdc931b362af9e2d9c6bcda3a84f51a5c
Richard Almonte <richard.almonte@infosurv.com>
2014-06-18 14:48:08 -0400

    add host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,8 @@ DogHouse::Application.configure do
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
+  config.action_mailer.default_url_options = { host: "komondor.herokuapp.com" }
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 end
